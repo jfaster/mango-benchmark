e0198364b86e998cf64f8dc1bd32bf97eaec5a52
ash <liangyanghe@gmail.com>
2014-06-03 18:53:13 +0800

    修复类名小写的问题

---
+++ src/main/java/cc/concurrent/mango/benchmark/dao/MangoUserDao.java
@@ -0,0 +1,16 @@
+package cc.concurrent.mango.benchmark.dao;
+
+import cc.concurrent.mango.DB;
+import cc.concurrent.mango.SQL;
+import cc.concurrent.mango.benchmark.User;
+
+/**
+ * @author ash
+ */
+@DB
+public interface MangoUserDao {
+
+    @SQL("insert into user_m(uid, name, money, create_time) values(:1.uid, :1.name, :1.money, :1.createTime)")
+    public int insert(User user);
+
+}
